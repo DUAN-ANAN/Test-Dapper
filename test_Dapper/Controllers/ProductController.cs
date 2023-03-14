a33f94f4eee7e77d8c2b7260a2ecb67e8f8def78
Andy <andyecgt@gmail.com>
2023-03-14 13:12:28 +0800

    加入專案檔案。

---
+++ test_Dapper/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using test_Dapper.Dapper;
+using test_Dapper.Models;
+
+namespace test_Dapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<Product> Product()
+        {
+            var d = new DapperProduct();
+            var r = d.GetProduct();
+            return r;
+        }
+
+        //[HttpGet("GetProductById/{ProductID}")] // 指定路由名稱
+        [HttpGet("[action]/{ProductID}")] // 會對應下面的action name --> appl
+        public IEnumerable<Product> Product(string ProductID)
+        {
+            var d = new DapperProduct();
+            var r = d.GetProductById(ProductID);
+            return r;
+        }
+    }
+}
---
+++ test_Dapper/Dapper/DapperProduct.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using test_Dapper.Models;
+using Dapper;
+
+namespace test_Dapper.Dapper
+{
+    public class DapperProduct
+    {
+        private readonly string _connString = @"Server=localhost\SQLEXPRESS;Database=AdventureWorksLT2019;User Id=sa;Password=andy54088;Trusted_Connection=True;"; //需放到appsetting
+
+
+        public IEnumerable<Product> GetProduct()
+        {
+            var sql = @"SELECT * FROM [SalesLT].[Product]";
+            using (var conn = new SqlConnection(_connString))
+            {
+                var result = conn.Query<Product>(sql);
+                return result;
+            }
+        }
+
+        public IEnumerable<Product> GetProductById(string ProductID)
+        {
+            var sql = @"SELECT * FROM [SalesLT].[Product] WHERE ProductID = @ProductID";
+            
+            //新增參數對應
+            var para = new DynamicParameters();
+            para.Add("ProductID", ProductID, System.Data.DbType.Int32);
+
+            using (var conn = new SqlConnection(_connString))
+            {
+                var result = conn.Query<Product>(sql, para);
+                return result;
+            }
+        }
+    }
+}
